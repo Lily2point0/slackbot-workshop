455821f769a423122fd93aaf838a8a39b6c2ca88
Lily2point0 <Lily2point0@users.noreply.github.com>
2019-03-05 09:54:50 +0000

    Update bot path

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 if (process.env.NODE_ENV !== 'production') require('dotenv').config();
-const bot = require('./bin/bot').init('BotName');
+const bot = require('./bin/lib/bot').init('BotName');
 
 console.log('App is running');
